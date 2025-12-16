7c1cbabe24e36029efc4435bfcd79b278659b363
Charbel Sahyoun <codecharbel@gmail.com>
2025-12-16 14:59:32 +0200

    Made sure to use the makefile command once and made a quick checkup on the code so that it works as intended

---
+++ ft_printf_Home/README.md
@@ -0,0 +1,18 @@
+This project has been created as part of the 42 curriculum by csahyoun
+
+# ft_printf
+
+## Description
+
+`ft_printf` is a custom implementation of the standard C library function `printf`.
+The goal of this project is to reproduce the behavior of `printf` for a subset of format specifiers, while gaining a deeper understanding of variadic functions, parsing, and formatted output in C.
+
+This library provides the function:
+
+```c
+int ft_printf(const char *format, ...);
+```
+
+# Instructions
+
+To run the ft_printf source run
--- ft_printf_Home/ft_printf.h
+++ ft_printf_Home/ft_printf.h
@@ -14,14 +14,16 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include "libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
 
-int	ft_printf(const char *start, ...);
-int	ft_put_char(int c);
-int	ft_put_string(char *s);
-int	ft_put_nbr(int n);
-int	ft_put_unb(unsigned int n);
-int	ft_decToHex (unsigned int nb, char c);
-int	ft_print_ptr(unsigned long long nb);
+int		ft_printf(const char *start, ...);
+int		ft_put_char(int c);
+int		ft_put_string(char *s);
+int		ft_put_nbr(int n);
+int		ft_put_unb(unsigned int n);
+int		ft_decToHex (unsigned int nb, char c);
+int		ft_print_ptr(unsigned long long nb);
+char	*ft_itoa(int n);
 
 #endif
